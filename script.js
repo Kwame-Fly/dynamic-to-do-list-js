23fc5494a42dabef5845d57ea1f72880403550c9
kwame takyi-kodua <kwametkodua@gmail.com>
2024-10-13 14:38:51 +0000

    added braces

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
    
     const addButton = document.getElementById("Add-Task");
-    const taskInput = document.getElementById('task-input');
-    const taskList = document.getElementById('task-list');
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
 
     
     function addTask() {
